6a526d4d8a1bd434ea7cc8ac2d9a6f82dfc3c491
yash10019coder <lit2020066@iiitl.ac.in>
2021-08-20 13:34:34 +0530

    Added test file for ImageViewBindingAdapters.java

---
+++ app/src/sharedTest/java/org/oppia/android/app/databinding/ImageViewBindingAdaptersTest.kt
@@ -0,0 +1,34 @@
+package org.oppia.android.app.databinding
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import java.lang.Exception
+
+class ImageViewBindingAdaptersTest {
+  @Before
+  @Throws(Exception::class)
+  fun setUp() {
+  }
+
+  @After
+  @Throws(Exception::class)
+  fun tearDown() {
+  }
+
+  @Test
+  fun setImageDrawable() {
+  }
+
+  @Test
+  fun testSetImageDrawable() {
+  }
+
+  @Test
+  fun testSetImageDrawable1() {
+  }
+
+  @Test
+  fun setProfileImage() {
+  }
+}
